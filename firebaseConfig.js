f1061433e3f126397f99b63d4d10b23af24f2903
Paul Martin McNeill <paulmcneill1989@hotmail.co.uk>
2025-02-28 23:59:14 +0000

    Add authentication context and update firebase configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,19 @@
 // firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 
-// from your Firebase console > Project settings
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_APP.firebaseapp.com',
-  databaseURL: 'https://YOUR_APP.firebaseio.com',
-  projectId: 'YOUR_APP',
-  storageBucket: 'YOUR_APP.appspot.com',
+  apiKey: 'AIzaSyBZS_Bfl7Bj4axlFt8Pg3HebYzAbrqBDQs',
+  authDomain: 'commai-b98fe.firebaseapp.com',
+  // Updated databaseURL:
+  databaseURL: 'https://commai-b98fe-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'commai-b98fe',
+  storageBucket: 'commai-b98fe.appspot.com',
   messagingSenderId: '...',
   appId: '...'
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
