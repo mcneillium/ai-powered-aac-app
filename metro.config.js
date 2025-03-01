99e30a77fb2d347415abcf9f6363580e6d70feee
Paul Martin McNeill <paulmcneill1989@hotmail.co.uk>
2025-03-01 01:33:46 +0000

    Update metro.config.js to support additional file extensions and map problematic modules

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,25 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+// Add "mjs" to the list of source file extensions
+defaultConfig.resolver.sourceExts = [
+  ...defaultConfig.resolver.sourceExts,
+  'mjs',
+  'jsx',
+  'js',
+  'ts',
+  'tsx',
+  'json'
+];
+
+// Keep your existing asset extensions
 defaultConfig.resolver.assetExts.push('bin');
-defaultConfig.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
+
+// Map problematic modules to an empty module if needed
+defaultConfig.resolver.extraNodeModules = {
+  ...defaultConfig.resolver.extraNodeModules,
+  'idb': require.resolve('./empty-module.js'),
+  './postinstall.mjs': require.resolve('./empty-module.js'),
+};
 
 module.exports = defaultConfig;
